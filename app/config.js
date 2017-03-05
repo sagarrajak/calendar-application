029ff07d8863443f25af7f7cd57433483a0aa3f6
sagarrajak <sagarrjak858@gmail.com>
2017-03-05 19:19:40 +0530

    changed file

---
+++ app/config.js
@@ -0,0 +1,7 @@
+/**
+ * Created by SAGAR on 3/5/2017.
+ */
+module.exports={
+    "database" :  "mongodb://sagarrajak:sagar123@ds119380.mlab.com:19380/calendar-app",
+    "port" : 3000||process.env.port
+}
