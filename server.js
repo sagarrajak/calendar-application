ad785d60d242535c3c4ec4232b6ee93a9175a4ac
sagarrajak <sagarrjak858@gmail.com>
2017-03-05 17:10:24 +0530

    modifying server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require('express');
 var body_parser = require('body-parser');
 var app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/story',function(err){
+mongoose.connect('mongodb://sagarrajak:sagar123@ds145379.mlab.com:45379/calendar-app',function(err){
     if(err)
         console.log(err);
     else
