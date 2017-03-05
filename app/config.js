fbbcde6390127565a4864741c3ac75a9d149c4d6
sagarrajak <sagarrjak858@gmail.com>
2017-03-05 21:29:48 +0530

    kjdfjfd

--- app/config.js
+++ app/config.js
@@ -2,6 +2,8 @@
  * Created by SAGAR on 3/5/2017.
  */
 module.exports={
-    "database" :  "mongodb://sagarrajak:sagar123@ds119380.mlab.com:19380/calendar-app",
+
+    "database" : "mongodb://sagarrajak:sagar123@ds119380.mlab.com:19380/calendar-app",
     "port" : 3000||process.env.PORT
+
 }
