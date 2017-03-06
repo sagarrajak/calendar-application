21c6de3b4805128d39b3d88cb63c76b3169b30cd
sagarrajak <sagarrjak858@gmail.com>
2017-03-06 19:24:09 +0530

    may be run

--- server.js
+++ server.js
@@ -8,13 +8,8 @@ var body_parser = require('body-parser');
 var config = require('../simple_story_app/app/config');
 var app = express();
 
-mongoose.connect(config.database,function(err){
-    if(err)
-        console.log(err);
-    else
-        console.log("connected to database");
-});
-
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 app.use(body_parser.urlencoded());
 app.use(body_parser.json());
@@ -24,9 +19,51 @@ app.use(express.static(__dirname));
 app.get('*',function(req,res){
     res.sendFile( __dirname + '/app/frontend/template/index.html' );
 });
-app.listen( config.port ,function(err){
+mongoose.connect(config.database,function(err){
+    if(err)
+        console.log(err);
+    else
+        console.log("connected to database");
+});
+server.listen( normalizePort(config.port) ,function(err){
     if(err)
         console.log(err);
     else
         console.log('server is running');
 });
+server.on('error',onError);
+
+
+function normalizePort(val){
+    var port =  parseInt(val,10);
+    if(isNaN(port))
+         return val;
+    if(port>=0)
+         return port;
+    return false;
+}
+
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
